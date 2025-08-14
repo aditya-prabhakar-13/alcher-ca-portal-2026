3f868798d9f294fa877d5e46a43df68395c5fb78
Gaurav Verma <wokenop@gmail.com>
2025-08-15 01:23:58 +0530

    register_group

--- apps/users/forms.py
+++ apps/users/forms.py
@@ -6,30 +6,29 @@ from django.contrib.auth import get_user_model
 from phonenumber_field.formfields import PhoneNumberField
 User = get_user_model()
 
-
 class SingleUserRegisterForm(UserCreationForm):
     email = forms.EmailField(widget=forms.TextInput(
-        attrs={'class': 'input_field', 'placeholder': 'Email *'}))
+        attrs={'class': 'input_field', 'placeholder': 'Enter Email*'}), required=True)
     firstname = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Full Name *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Enter Full Name*'}), required=True)
     referred_by = forms.CharField(required=False, widget=forms.TextInput(
         attrs={'class': 'input_field', 'placeholder': 'Enter referral id'}))
     phone = forms.CharField(widget=forms.TextInput(attrs={
-                             'class': 'input_field', 'placeholder': 'Phone Number (e.g. +12125552368) *'}), label="Phone number (e.g. +12125552368)", )
+                             'class': 'input_field', 'placeholder': 'Enter Phone Number*'}), label="PhoneNumber", required=True)
 
     graduation_year = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Graduation Year *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Graduation Year*'}), required=True)
     college_city = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College City *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College City*'}), required=True)
     college_state = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College State *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College State*'}), required=True)
     college_name = forms.CharField(
-        label="Full Name", widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College Name *'}))
+        label="Full Name", widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College Name*'}), required=True)
     
     password1 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'input_field', 'placeholder': 'Password *'}))
+        widget=forms.PasswordInput(attrs={'class': 'input_field', 'placeholder': 'Set Password*'}), required=True)
     password2 = forms.CharField(
-        widget=forms.PasswordInput(attrs={'class': 'input_field', 'placeholder': 'Confirm your Password *'}))
+        widget=forms.PasswordInput(attrs={'class': 'input_field', 'placeholder': 'Confirm Password*'}), required=True)
 
     def clean(self):
         cleaned_data = super(SingleUserRegisterForm, self).clean()
@@ -48,30 +47,29 @@ class SingleUserRegisterForm(UserCreationForm):
 
 class GroupUserRegisterFormForSingle(forms.ModelForm):
     email = forms.EmailField(widget=forms.TextInput(
-        attrs={'class': 'input_field', 'placeholder': 'Email *'}))
+        attrs={'class': 'input_field', 'placeholder': 'Enter Email ID*'}), required=True)
     firstname = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Full Name *'}))
-    # referred_by = forms.CharField(required=False, widget=forms.TextInput(
-    #     attrs={'class': 'input_field', 'placeholder': 'Enter referral id'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Enter Full Name*'}), required=True)
+
     phone = forms.CharField(widget=forms.TextInput(attrs={
-                             'class': 'input_field', 'placeholder': 'Phone Number (e.g. +12125552368) *'}), label="Phone number (e.g. +12125552368)", )
+                             'class': 'input_field', 'placeholder': 'Enter Phone Number*'}), label="Phone number", required=True )
 
     graduation_year = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Graduation Year *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'Graduation Year*'}), required=True)
 
     class Meta:
         model = User
-        fields = ['firstname', 'email', 'phone', 'graduation_year']
+        fields = ['firstname', 'phone', 'email', 'graduation_year']
 
 
 # incomplete
 class GroupUserRegisterForm(forms.ModelForm):
     college_city = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College City *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College City *'}), required=True)
     college_state = forms.CharField(
-        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College State *'}))
+        widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College State *'}), required=True)
     college_name = forms.CharField(
-        label="Full Name", widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College Name *'}))
+        label="Full Name", widget=forms.TextInput(attrs={'class': 'input_field', 'placeholder': 'College Name *'}), required=True)
     referred_by = forms.CharField(required=False, widget=forms.TextInput(
         attrs={'class': 'input_field', 'placeholder': 'Enter referral id'}))
 
